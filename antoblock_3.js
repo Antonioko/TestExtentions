8c9e6d68079ba7bfbf83ff9b821ce53c35f98c71
Antonioko <50264835+Antonioko@users.noreply.github.com>
2019-05-05 12:52:59 +0200

    1

---
+++ antoblock_3.js
@@ -0,0 +1,33 @@
+class AntoBlock {
+    getInfo() {
+        return {
+            "id": "AntoBlock",
+            "name": "AntoBlock",
+            "blocks": [{
+                    "opcode": "substringy",
+                    "blockType": "reporter",
+                    "text": "letters [num1] through [num2] of [string]",
+                    "arguments": {
+                        "num1": {
+                            "type": "number",
+                            "defaultValue": "2"
+                        },
+                        "num2": {
+                            "type": "number",
+                            "defaultValue": "5"
+                        },
+                        "string": {
+                            "type": "string",
+                            "defaultValue": "hello world"
+                        }
+                    }
+                },
+            }],
+        "menus": { //we will get back to this in a later tutorial
+        }
+    };
+    substringy({num1, num2, string}) {
+        return string.substring(num1 - 1, num2);
+    };
+}
+Scratch.extensions.register(new AntoBlock());
